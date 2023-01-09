b3c48840061c7bf21077750c5805a9e27c633ee7
fanetech <louguefranck.20@gmail.com>
2023-01-09 00:37:45 +0000

    allow required role to register

--- controllers/auth/user.auth.controller.js
+++ controllers/auth/user.auth.controller.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const { signUpErrors, signInErrors } = require("../../utils/utils.errors");
 const { getSchoolOfUser } = require("../school/school.controller");
 const bcrypt = require("bcrypt");
+const { USER_ROLE } = require("../../services/constant");
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
@@ -25,7 +26,7 @@ module.exports.login = async (req, res) => {
           number: number,
         },
       ],
-    });
+    })
 
     if (user) {
       const auth = await bcrypt.compare(password, user.password);
@@ -49,15 +50,20 @@ module.exports.login = async (req, res) => {
 module.exports.register = async (req, res) => {
   if (Object.keys(req.body).length === 0)
     return res.status(400).json({ msg: "error", err: "No data" });
-  const { number, email, password } = req.body;
-  if ((!number || !email) && !password) {
+  const { number, email, password, role } = req.body;
+  if ((!number || !email) && !password && !role) {
     return res.status(400).json({ msg: "error", err: "data no complete" });
+  }
+  const _role = USER_ROLE[role]
+  if (!_role) {
+    return res.status(400).json({ msg: "error", err: "role incorect" });
   }
   try {
     const user = await userModel.create({
       number,
       email,
       password,
+      role
     });
     res.status(200).json({ msg: "success", user: user });
   } catch (err) {
